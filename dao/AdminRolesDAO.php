0142f76d40ebc8d32d359369bc66d4ec0e938d76
Jasper Van Damme <jasper.van.damme@student.howest.be>
2016-06-01 22:24:35 +0200

    Fixed typo at update query

--- dao/AdminRolesDAO.php
+++ dao/AdminRolesDAO.php
@@ -37,7 +37,7 @@ class AdminRolesDAO extends DAO {
 	}
 
 	public function update($id, $title, $can_create_admin, $can_approve_groups, $can_edit_creations, $can_delete_creations, $can_feature_creations, $can_judge_creations) {
-		$sql = "UPDATE `tt_admin_roles` `title` = :title, `can_create_admin` = :can_create_admin, `can_approve_groups` = :can_approve_groups, `can_edit_creations` = :can_edit_creations, `can_delete_creations` = :can_delete_creations, `can_feature_creations` = :can_feature_creations, `can_judge_creations` = :can_judge_creations WHERE `id` = :id";
+		$sql = "UPDATE `tt_admin_roles` SET `title` = :title, `can_create_admin` = :can_create_admin, `can_approve_groups` = :can_approve_groups, `can_edit_creations` = :can_edit_creations, `can_delete_creations` = :can_delete_creations, `can_feature_creations` = :can_feature_creations, `can_judge_creations` = :can_judge_creations WHERE `id` = :id";
 		$qry = $this->pdo->prepare($sql);
 		$qry->bindValue(':id', $id);
 		$qry->bindValue(':title', $title);

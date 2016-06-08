0df2975bdaf33e2f115bf6e6ac664af645139ff5
Jasper Van Damme <jasper.van.damme@student.howest.be>
2016-06-08 21:47:07 +0200

    dependencies should be saved in bower

--- _js/script.js
+++ _js/script.js
@@ -1,6 +1,9 @@
 'use strict';
 
 import 'es5-shim';
+import 'backbone.validation';
+/*import 'bootstrap';
+import 'bootstrap-formhelpers';*/
 import Backbone from 'backbone';
 import AppRouter from './router/AppRouter';
 import 'helpers';
